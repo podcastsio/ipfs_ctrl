9394d81f6d3d83ba7dffbc9ac4284b9697090e89
Andrew Nesbitt <andrewnez@gmail.com>
2020-09-30 14:11:29 +0100

    pushes

--- server.rb
+++ server.rb
@@ -32,6 +32,10 @@ post '/archive' do
   return `cd #{directory_path} && hub buck init --name=#{params[:bucket]} --private=false --thread=#{params[:thread_id]} -q && hub buck push -y -f -q && hub buck archive -y`
 end
 
+post '/push' do
+  return `cd /data/podcasts/#{params[:bucket]} && hub buck push -y -f`
+end
+
 post '/retry_archive' do
-  return `cd /data/podcasts/#{params[:bucket]} && hub buck archive -y`
+  return `cd /data/podcasts/#{params[:bucket]} && hub buck push -y -f && hub buck archive -y`
 end
